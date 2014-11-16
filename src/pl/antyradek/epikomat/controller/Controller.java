ae8f62a39fc777a72685da0df841c8676fe24ed3
Radosław Świątkiewicz <antyradek@gmail.com>
2014-11-16 01:03:47 +0100

    Create projest

---
+++ src/pl/antyradek/epikomat/controller/Controller.java
@@ -0,0 +1,26 @@
+package pl.antyradek.epikomat.controller;
+
+import pl.antyradek.epikomat.debug.*;
+
+/**
+ * Główny kontroler całego programu. Ma najszerszą wiedzę, ale sam mało robi
+ * poza przesyłaniem zapytań z prawa na lewo.
+ * 
+ * @author Radosław Świątkiewicz
+ *
+ */
+public class Controller
+{
+
+	/**
+	 * Główne wystartowanie aplikacji
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Debug.log("Witamy wszystkich");
+
+	}
+
+}
---
+++ src/pl/antyradek/epikomat/debug/Debug.java
@@ -0,0 +1,13 @@
+package pl.antyradek.epikomat.debug;
+/**
+ * Klasa służąca głowne do wypisywania informacji do logu (do terminala)
+ * @author Radosław Świątkiewicz
+ *
+ */
+public class Debug 
+{
+	public static void log(String message)
+	{
+		System.out.println(message);
+	}
+}
