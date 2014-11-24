9a707af2a52099e53a221ca542e3e53f189a1e6d
Radosław Świątkiewicz <antyradek@gmail.com>
2014-11-25 00:48:19 +0100

    Play with Resources

--- src/pl/antyradek/epikomat/debug/Debug.java
+++ src/pl/antyradek/epikomat/debug/Debug.java
@@ -6,8 +6,22 @@ package pl.antyradek.epikomat.debug;
  */
 public class Debug 
 {
+	/**
+	 * Wypisuje tekst do terminala
+	 * @param message
+	 */
 	public static void log(String message)
 	{
 		System.out.println(message);
+	}
+	
+	/**
+	 * Wypisuje tekst do terminala, ale na czerwono!
+	 * @param message
+	 */
+	public static void logErr(String message)
+	{
+		//ANSI escape codes
+		System.out.println("\033[31m" + message + "\033[0m");
 	}
 }
--- src/pl/antyradek/epikomat/resources/Resources.java
+++ src/pl/antyradek/epikomat/resources/Resources.java
@@ -1,5 +1,11 @@
 package pl.antyradek.epikomat.resources;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.PropertyResourceBundle;
+
+import pl.antyradek.epikomat.debug.Debug;
+
 /**
  * Zasoby językowe, wszystkich tekstów i innych. Będą pobierane z pliku. 
  * Na podobnej zasadzie działa Android.
@@ -9,12 +15,42 @@ package pl.antyradek.epikomat.resources;
 public final class Resources
 {
 	/**
+	 * Czy całe urządzenie działa?
+	 */
+	private static boolean successful; 
+	/**
+	 * Bundle obsługujący wszystko
+	 */
+	private static PropertyResourceBundle bundle;
+	
+	/**
+	 * Nazwa pliku zawierającego wszystkie dane
+	 */
+	private static final String resourceBundleFilename = "resources_pl"; //FIXME: Zmienić na wiele języków
+	
+	
+	/**
 	 * Czytanie z pliku zasobów
 	 */
 	static
 	{
-		//TODO: Zrobić
+		try
+		{
+			//czytanie pliku
+			bundle = new PropertyResourceBundle(new FileInputStream(resourceBundleFilename));
+			successful = true;
+		} catch (IOException e)
+		{
+			successful = false;
+			bundle = null;
+			Debug.logErr("Nie znaleziono zasobów!");
+		}
+		
 	}
-	//public static String getResource()
+	
+	public static String getWindowName()
+	{
+		//TODO: jak to zrobić? Chyba lepiej przez jedną metodę
+	}
 
 }
